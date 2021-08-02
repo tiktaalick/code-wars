30f693206f585cd45458977c0b91c8bde1fd22e2
tiktaalick <blogfeedser@gmail.com>
2021-08-03 00:27:35 +0200

    Added an alternative to ArrayDiff.java

--- src/main/java/kata/kyu6/ArrayDiff.java
+++ src/main/java/kata/kyu6/ArrayDiff.java
@@ -3,6 +3,7 @@ package kata.kyu6;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayDiff {
     private ArrayDiff() {
@@ -18,5 +19,12 @@ public class ArrayDiff {
         });
 
         return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static int[] alternative(int[] a, int[] b) {
+        List<Integer> listA = Arrays.stream(a).boxed().collect(Collectors.toList());
+        List<Integer> listB = Arrays.stream(b).boxed().collect(Collectors.toList());
+        listA.removeAll(listB);
+        return listA.stream().mapToInt(e -> e).toArray();
     }
 }
--- src/test/java/kata/kyu6/ArrayDiffTest.java
+++ src/test/java/kata/kyu6/ArrayDiffTest.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class ArrayDiffTest {
 
     @Test
-    void justDoIt() {
+    void testJustDoIt() {
         assertArrayEquals(new int[]{2}, ArrayDiff.justDoIt(new int[]{1, 2}, new int[]{1}));
         assertArrayEquals(new int[]{2, 2}, ArrayDiff.justDoIt(new int[]{1, 2, 2}, new int[]{1}));
         assertArrayEquals(new int[]{1}, ArrayDiff.justDoIt(new int[]{1, 2, 2}, new int[]{2}));
         assertArrayEquals(new int[]{1, 2, 2}, ArrayDiff.justDoIt(new int[]{1, 2, 2}, new int[]{}));
         assertArrayEquals(new int[]{}, ArrayDiff.justDoIt(new int[]{}, new int[]{1, 2}));
+    }
+
+    @Test
+    void testAlternative() {
+        assertArrayEquals(new int[]{2}, ArrayDiff.alternative(new int[]{1, 2}, new int[]{1}));
+        assertArrayEquals(new int[]{2, 2}, ArrayDiff.alternative(new int[]{1, 2, 2}, new int[]{1}));
+        assertArrayEquals(new int[]{1}, ArrayDiff.alternative(new int[]{1, 2, 2}, new int[]{2}));
+        assertArrayEquals(new int[]{1, 2, 2}, ArrayDiff.alternative(new int[]{1, 2, 2}, new int[]{}));
+        assertArrayEquals(new int[]{}, ArrayDiff.alternative(new int[]{}, new int[]{1, 2}));
     }
 }
